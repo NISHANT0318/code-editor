c7e4aa3b9a41db52021cc24185939ace360118c9
Nishant <Nishantsatone1997@gmail.com>
2024-06-11 09:59:31 +0530

    Functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,68 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Editor from './components/Editor'
 
 function App() {
+
+  const [html,setHtml]= useState('')
+  const [css,setCss]= useState('')
+  const [js,setJs]= useState('')
+  const [srcDoc,setSrcDoc]=useState('')
+
+
+  useEffect(()=>{
+
+    const timeout = setTimeout(() => {
+
+      setSrcDoc(
+  `
+  <html>
+  <body>${html}</body>
+  <style>${css}</style>
+  <script>${js}</script>
+  </html>  
+      `)
+      
+    }, 250);
+
+    return () => clearTimeout(timeout)
+
+  },[html,css,js])
+  
   
 
   return (
     <>
       <div className='pane top-pane'>
-        <Editor/>
-        <Editor/>
-        <Editor/>
+        <Editor 
+        language="xml"
+        displayName="HTML"
+        value={html}
+        onChange={setHtml}
+        />
+        <Editor 
+        language="css"
+        displayName="CSS"
+        value={css}
+        onChange={setCss}/>
+        <Editor 
+        language="js"
+        displayName="JS"
+        value={js}
+        onChange={setJs}
+        />
       </div>
       <div className='pane'>
         <iframe 
+        srcDoc={srcDoc}
          title='output'
-         sandbox='allow-script'
+         sandbox='allow-scripts'
          width="100%"
          height="100%"
-         frameborder="0"></iframe>
-      </div>
+         frameBorder="0" 
+         />
+       </div>
+      
     </>
   )
 }
